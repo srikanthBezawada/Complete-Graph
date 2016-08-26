1a7307cb690341d65217cf2c3aa08e54920b9551
srikanthBezawada <srikanth.5283@gmail.com>
2016-08-26 21:01:39 +0400

    Wraps the computation into a task, all set for release

--- Complete-Graph/src/main/java/org/cytoscape/completegraph/internal/CytoscapeAppActivator.java
+++ Complete-Graph/src/main/java/org/cytoscape/completegraph/internal/CytoscapeAppActivator.java
@@ -4,8 +4,11 @@ import java.util.Properties;
 
 import org.osgi.framework.BundleContext;
 
-import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.service.util.AbstractCyActivator;
+import org.cytoscape.service.util.CyServiceRegistrar;
+import static org.cytoscape.work.ServiceProperties.PREFERRED_MENU;
+import static org.cytoscape.work.ServiceProperties.TITLE;
+import org.cytoscape.work.TaskFactory;
 
 
 /**
@@ -25,9 +28,12 @@ public class CytoscapeAppActivator extends AbstractCyActivator {
     
     public void start(BundleContext context) throws Exception {
         this.context = context;
-        CyApplicationManager cyApplicationManager = getService(context, CyApplicationManager.class);
-        CompleteGraphAction oplaction = new CompleteGraphAction(this, cyApplicationManager);
-        registerAllServices(context, oplaction, new Properties());
+        final CyServiceRegistrar serviceRegistrar = getService(context, CyServiceRegistrar.class);
+        final KGraphTaskFactory kGraphFactory = new KGraphTaskFactory(serviceRegistrar);
+        Properties kGraphProps = new Properties();
+        kGraphProps.setProperty(PREFERRED_MENU, "File.New.Network");
+        kGraphProps.setProperty(TITLE, "Complete Network");
+        registerService(context, kGraphFactory, TaskFactory.class, kGraphProps);
     }
     
     public <S> S getService(Class<S> cls) {
